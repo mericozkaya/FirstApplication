c906787a05cface5162fdddbf0b661d5aa0833b2
Meriç Özkaya <mericozkaya@outlook.com>
2024-07-14 20:03:17 +0300

    Initial commit

---
+++ app/src/main/java/com/mericozkaya/firstapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.mericozkaya.firstapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeImage (View view){
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.image2);
+
+    }
+
+
+}
